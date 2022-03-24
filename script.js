ad2d0febdc855ea7dd1ebac1ceac9bb4ec6f931b
Leonardo Bernardes <lsbernardes@gmail.com>
2022-03-24 06:02:43 +0100

    terceiro commit

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const acrescentar = document.querySelector('.btn-1');
-const background = document.querySelector('.box-bg');
+const overlay = document.querySelector('.overlay');
+const box = document.querySelector('.box');
+const closeBtn = document.querySelector('.close');
 
-acrescentar.addEventListener('click', () => {
-  background.classList.remove('hidden');
-});
+const abrirModal = () => {
+  overlay.classList.remove('hidden');
+  box.classList.remove('hidden');
+};
+const fecharModal = () => {
+  overlay.classList.add('hidden');
+  box.classList.add('hidden');
+};
+
+acrescentar.addEventListener('click', abrirModal);
+closeBtn.addEventListener('click', fecharModal);
+overlay.addEventListener('click', fecharModal);
